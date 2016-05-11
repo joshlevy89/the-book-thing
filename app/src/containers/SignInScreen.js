56459bc70b8df515d501cc1b33c6f6994bf50fc8
Josh Levy <joshlevy89@gmail.com>
2016-05-11 18:51:57 -0400

    modified the authentication link in the client for the production app

--- app/src/containers/SignInScreen.js
+++ app/src/containers/SignInScreen.js
@@ -15,7 +15,7 @@ class SignInScreen extends Component {
     
 render() {
     var isProduction = process.env.NODE_ENV === 'production';
-    if (isProduction) var url = 'https://my-nightlife-coordinator.herokuapp.com/';
+    if (isProduction) var url = 'https://the-book-thing.herokuapp.com/';
     else var url = 'http://127.0.0.1:3000/';
     return (
          <div className="mainLayout" style={{'text-align':'center', 'marginTop':'100px'}}>
