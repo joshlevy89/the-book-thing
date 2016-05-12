161fd2b07a63c49bbfa2e84c216358d6067d9651
Josh Levy <joshlevy89@gmail.com>
2016-05-11 22:44:10 -0400

    fixed typeError when trades array undefined

--- app/src/containers/TradeOffersScreen.js
+++ app/src/containers/TradeOffersScreen.js
@@ -8,9 +8,17 @@ require('../../styles/index.scss');
 class TradeOffersScreen extends Component {
     render() {
         const { trade_offers } = this.props
+        if (trade_offers === undefined) {
+            return (<div></div>)
+        }
         return (
         <div style={{'marginLeft':'10px'}}>
         <h2 className="mainLayout">Trade Offers</h2>
+        {trade_offers.length===0 ? 
+            <div style={{'marginLeft': '20px'}} className="mainLayout">
+            No Trades Have Been Offered To You</div>:
+            <div></div>
+        }
         {trade_offers.map(mytrade=>{
             return (
             <div style={{'margin-bottom':'10px'}} key={mytrade._id}>
