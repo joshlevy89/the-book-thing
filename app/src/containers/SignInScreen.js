410fd79ab6a9062e4ea8c1bef7e9cf809e83dab6
Josh Levy <joshlevy89@gmail.com>
2016-05-13 09:39:48 -0400

    added back signin button even after signin

--- app/src/containers/SignInScreen.js
+++ app/src/containers/SignInScreen.js
@@ -16,17 +16,19 @@ render() {
     return (
          <div className="mainLayout" style={{'textAlign':'center', 'marginTop':'100px'}}>
             <h2 style={{"marginBottom":'20px'}}>The Book Thing</h2>
-            {username === undefined ? 
-            (<a href={url+'auth/twitter/'}>
+            <a href={url+'auth/twitter/'}>
             <Button bsStyle="primary" bsSize="lg">Sign in with Twitter
             <i style={{'marginLeft':'5px'}} className="fa fa-twitter"></i>
             </Button>
-            </a>):
-            (<div>
-            <Button onClick={()=>browserHistory.push('/mybooks')} bsStyle="primary" bsSize="lg">View My Books
+            </a>
+            {username === undefined ? 
+            (
+            <div>
+            <Button style={{'marginTop':'20px'}} bsStyle="success" bsSize="lg" onClick={()=>browserHistory.push('/mybooks')}>
+            View My Books
             </Button>
             </div>
-            )}
+            ):(<div></div>)}
             <div>
             <h3>Trade books with people from all over</h3>
             </div>
